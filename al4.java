540fff98ee207f9be36c4d3df69244f51e13a968
kaivalya <kaivalya.kaivi>
2023-09-22 14:46:43 +0530

    sort arraylist

---
+++ al4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class al4{
+    public static void main(String args[]){
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        al.add(213);
+        al.add(8);
+        al.add(35);
+        al.add(200);
+        Collections.sort(al);
+        System.out.println(al);
+
+    
+    }
+}
