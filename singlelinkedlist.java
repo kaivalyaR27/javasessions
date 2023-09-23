9fdbce8d39a7a4c964f4fa836d8dfb13c25094fc
kaivalya <kaivalya.kaivi>
2023-09-23 11:51:54 +0530

    linkedlist programs

---
+++ singlelinkedlist.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class singlelinkedlist{
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public Node head=null;
+    public Node tail=null;
+    public void addNode(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+        }
+        else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    public void display(){
+        Node current=head;
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        System.out.println("Nodes of ssl");
+        while(current!=null){
+            System.out.print(current.data+" ");
+            current=current.next;
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        singlelinkedlist sl=new singlelinkedlist();
+        sl.addNode(3);
+        sl.addNode(9);
+        sl.addNode(22);
+        sl.display();
+    }
+}
